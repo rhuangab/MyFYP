06b7676dbc8dafd584dfb4db85baefca15da732a
rhuangab <rhuangab@ust.hk>
2013-11-14 22:13:14 +0800

    Fix the crawler.

--- MyFYP/src/Phonetic/Retriever.java
+++ MyFYP/src/Phonetic/Retriever.java
@@ -3,10 +3,17 @@ package Phonetic;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.filters.HasParentFilter;
+import org.htmlparser.filters.OrFilter;
 import org.htmlparser.filters.TagNameFilter;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
@@ -18,74 +25,61 @@ public class Retriever {
 	private String m_type;
 	private int firstEmphasis;
 	private int secondEmphasis;
+	private List<String> dataList;
 
 	public Retriever(String word, String type) throws ParserException
 	{
-		int i = 1;
+		dataList = new ArrayList();
 		m_word = word;
 		//1 is default value, meaning the first choice of a word with multiple meanings
 		try{
-		retrieve(i);
 		//m_type = type;
-		while(type != null && !m_type.equals(type))
-		{
-			retrieve(++i);
-		}
+		retrieve();
 		}
 		catch(NullPointerException e)
 		{
-			System.out.println("*Exception for catching phonetic for " + word + " with type "+type+".");
+			System.out.println("*Exception for catching phonetic for " + word + ".");
+		}
+		catch(ParserException e)
+		{
+			System.out.println("*Exception for catching phonetic for " + word + ".");
 		}
 		/** use this data structure to store in the database */
-		WordStruct wordStruct = new WordStruct(m_word,m_phonetic,m_type, firstEmphasis, secondEmphasis);
-		
+		WordStruct wordStruct = new WordStruct(m_word,dataList);
 	}
-
-	public void retrieve(int i) throws ParserException,NullPointerException
+	/*
+	public boolean isPhonetic(String input)
 	{
-		/*final WebClient webClient = new WebClient();
-		//HtmlPage page = webClient.getPage("http://www.merriam-webster.com/dictionary/"+m_word);
-		HtmlPage page = webClient.getPage("http://www.learnersdictionary.com/definition/hello");
-		HtmlDivision headword = (HtmlDivision) page.getElementById("headword");
-		System.out.println(headword.getChildElementCount());*/
-		/*DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		InputStream io = new URL("http://www.merriam-webster.com/dictionary/hello").openStream();
-		Document doc = dBuilder.parse(io);
-		Element e = doc.getElementById("headword");
-		System.out.println("1"+e.getNodeType());*/
-		
-		//String s = dBuilder.parse("http://www.merriam-webster.com/dictionary/hello").toString();
-		//doc.getElementById(elementId)
-		//HtmlDivision headword = (HtmlDivision) doc.getElementById("headword");
-		//System.out.println(headword.getChildElementCount());
-		//URLConnection uc = new URL("http://www.merriam-webster.com/dictionary/hello").openConnection();
-		
-		//HtmlPage htmlPage = new HtmlPage(parser);
-		//System.out.println((htmlPage.getTitle()));
-		//Node node = (Node)parser.extractAllNodesThatMatch(new TagNameFilter ("title")).elementAt(0);
-		//org.htmlparser.filters.CssSelectorNodeFilter
-		
-		// String b = wordType.toPlainTextString();
-		/*
-		 * NodeList children = parent.getChildren(); for(int i =0; i <
-		 * children.size();i++) {
-		 * System.out.println(i+": "+children.elementAt(i).toPlainTextString()); }
-		 */
-		// System.out.println(.toPlainTextString());
-		// System.out.println(b);
-		// System.out.println(a.contains("\u0712"));
-		// System.out.println((int)(a.charAt(1)));
-		// System.out.println("\u7686");
-		// System.out.println(node.toPlainTextString());
-		//String url = "http://www.learnersdictionary.com/definition/" + m_word;
+		if(input != null && input.contains("/"))
+			return true;
+		else
+			return false;
+	}*/
+
+	public void retrieve() throws ParserException,NullPointerException
+	{
 		String url = "http://www.merriam-webster.com/dictionary/" + m_word;
-		if(i > 1)
-			url += "["+i+"]";
 		Parser parser = new Parser();
 		parser.setURL(url);
-		NodeList nodes = parser.extractAllNodesThatMatch(new TagNameFilter("span"));
-		Node node = nodes.elementAt(1);
+		//NodeList nodes = parser.extractAllNodesThatMatch(new TagNameFilter("span"));
+		NodeFilter filter1 = new OrFilter(new HasAttributeFilter("class","main-fl"),new HasAttributeFilter("class","pr"));
+		NodeFilter filter2 = new HasParentFilter(new HasAttributeFilter("id","headword"));
+		NodeList typeNodes = parser.extractAllNodesThatMatch(new AndFilter(filter1, filter2));
+		//NodeList phoneticNodes = parser.extractAllNodesThatMatch();
+		System.out.println(m_word);
+		for(int i=0;i<typeNodes.size();i++)
+		{
+			System.out.println(typeNodes.elementAt(i).toPlainTextString());
+			dataList.add(typeNodes.elementAt(i).toPlainTextString());
+			
+			/*String currentContent = typeNodes.elementAt(i).toPlainTextString();
+			if(type != null && type.equals(typeNodes.elementAt(i).toPlainTextString()))				
+				if(isPhonetic(currentContent))
+					previousPhonetic = currentContent;*/
+		}
+		
+		/*
+		//Node node = nodes.elementAt(1);
 		Node parent = node.getParent();
 		Node wordType = parent.getLastChild().getPreviousSibling();
 		m_type = wordType.toPlainTextString();
@@ -95,6 +89,7 @@ public class Retriever {
 		 * For example, 'surprise', that has 3 meanings, but all three meaning hold the same phonetic
 		 * And the second and third meanings do not list out the phonetic, so we use the old phonetic.  		
 		*/
+		/*
 		if(node.toPlainTextString().contains("/"))
 			m_phonetic = node.toPlainTextString();
 		firstEmphasis = m_phonetic.indexOf("ˈ");
@@ -103,7 +98,7 @@ public class Retriever {
 		System.out.println(m_phonetic);
 		System.out.println("First emphasis: "+m_phonetic.indexOf("ˈ"));
 		System.out.println("Second emphasis: "+m_phonetic.indexOf("ˌ"));
-		System.out.println("Type: "+m_type);
+		System.out.println("Type: "+m_type);*/
 	}
 	
 	
@@ -114,14 +109,14 @@ public class Retriever {
 	 */
 	public static void main(String[] args) throws ParserException, FileNotFoundException {
 		// TODO Auto-generated method stub
-		//File file = new File("dictionary.txt");
-//		Scanner sc = new Scanner(file);
-//		while(sc.hasNext())
-//		{
-//			String word = sc.next();
-			Retriever retriever = new Retriever("present",null);
-//		}
-		//retriever.extractWords("http://www.learnersdictionary.com/definition/hello");
+		File file = new File("dictionary.txt");
+		Scanner sc = new Scanner(file);
+		while(sc.hasNext())
+		{
+				String word = sc.next();
+				Retriever retriever = new Retriever(word,null);
+		}
+		  //retriever.extractWords("http://www.learnersdictionary.com/definition/hello");
 	}
 
 }
--- MyFYP/src/Phonetic/WordStruct.java
+++ MyFYP/src/Phonetic/WordStruct.java
@@ -1,20 +1,23 @@
 package Phonetic;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class WordStruct implements Serializable{
 	public String word;
-	public String phonetic;
-	public String type;
-	public int firstEmphasis;
-	public int secondEmphasis;
+	//public String phonetic;
+	//public String type;
+	public List<String> dataList;
+	//public int firstEmphasis;
+	//public int secondEmphasis;
 
-	WordStruct(String word, String phonetic, String type, int fE, int sE)
+	WordStruct(String word, List<String> dataList/*String phonetic, String type, int fE, int sE*/)
 	{
 		this.word = word;
-		this.phonetic = phonetic;
-		this.type = type;
-		this.firstEmphasis = fE;
-		this.secondEmphasis = sE;
+		this.dataList = dataList;
+		//this.phonetic = phonetic;
+		//this.type = type;
+		//this.firstEmphasis = fE;
+		//this.secondEmphasis = sE;
 	}
 }
