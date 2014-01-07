74f07e9695ad2cde0159ad632def3d3f4742cba6
jenny <jenny@192.168.0.104>
2014-01-07 15:31:52 +0800

    Melody parser

---
+++ MyFYP/src/Melody/MelodyParser.java
@@ -0,0 +1,48 @@
+package Melody;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Synthesizer;
+import javax.sound.midi.Track;
+
+public class MelodyParser {
+	
+    public static Sequencer sequencer;
+    public static Sequence sequence;
+    
+    
+    public static void main(String[] args) throws Exception {
+    	
+    	sequencer = MidiSystem.getSequencer();
+        sequencer.open();
+        
+        sequence = MidiSystem.getSequence(new File("/Users/jenny/git/MyFYP/MyFYP/midiLibrary/Without You.mid"));
+        sequencer.setSequence(sequence);
+        
+        //Synthesizer synthesizer = MidiSystem.getSynthesizer();
+    	//synthesizer.open();
+        
+        sequencer.stop(); 
+        Midi myMidi = new Midi(sequencer, sequence);
+        //myMidi.infoSequence(true);
+        myMidi.SeperateChannel();
+        Sequence newSequence = myMidi.newSequence;
+        System.out.println("Old sequence information:");
+        myMidi.printTrackInformation(sequence);
+        System.out.println();
+        System.out.println("New sequence information:");
+        myMidi.printTrackInformation(newSequence);
+        //synthesizer.close();
+
+    }
+	
+}
+
