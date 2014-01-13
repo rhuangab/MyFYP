ea838a8a44bffca8b9c10caff45edf4ad43ac69b
richeng <jerrcwong@gmail.com>
2014-01-13 22:08:22 +0800

    Enhance the search tool.

--- MyFYP/src/Phonetic/checkAWordExistence.java
+++ MyFYP/src/Phonetic/checkAWordExistence.java
@@ -13,6 +13,17 @@ public class checkAWordExistence {
 		DatabaseManager.getHashtableSingleton().remove(word.toUpperCase());
 		DatabaseManager.commit();
 	}
+	
+	public static void addAWord(String wordAndValue) throws IOException
+	{
+		String[] words = wordAndValue.split("\\s+");
+		if(words.length == 3)
+		{
+			WordStruct wd = new WordStruct(words[0].toUpperCase(), words[1], words[2]);
+			DatabaseManager.getHashtableSingleton().put(words[0].toUpperCase(), wd);
+			DatabaseManager.commit();
+		}
+	}
 	/**
 	 * @param args
 	 * @throws IOException 
@@ -24,20 +35,26 @@ public class checkAWordExistence {
 		while(true)
 		{
 			System.out.print("Input a word: ");
-			String inputString = input.next();
-			if(inputString.equals("richeng"))
+			String inputString = input.nextLine();
+			if(inputString.equals("end"))
 			{
 				DatabaseManager.close();
 				break;
 			}
-			else if(inputString.charAt(0) == '_' && inputString.length() > 1)
+			else if(inputString.charAt(0) == '-' && inputString.length() > 1)
 			{
 				deleteAWord(inputString.substring(1));
 				continue;
 			}
+			else if(inputString.charAt(0) == '+' && inputString.length() > 1)
+			{
+				addAWord(inputString.substring(1));
+				continue;
+			}
+				
 			WordStruct wd = (WordStruct)hashtable.get(inputString.toUpperCase());
 			if(wd != null)
-				System.out.println(wd.word+"\nStress: "+wd.stress+"\nCv: "+wd.cv);
+				System.out.println(wd.word+" "+wd.stress+" "+wd.cv);
 			else
 				System.out.println("Word not found");
 		}
